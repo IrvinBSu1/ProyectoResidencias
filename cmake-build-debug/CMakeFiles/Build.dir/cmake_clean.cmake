6f2a38e118e13e2fd13071a950daf10d73a1f4e3
Irvin Baez Suarez <l17370668@apizaco.tecnm.mx>
2022-05-17 23:58:51 -0500

    Create Project

---
+++ cmake-build-debug/CMakeFiles/Build.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Build.ihx"
+  "Build.ihx.manifest"
+  "Build.pdb"
+  "CMakeFiles/Build.dir/src/main.c.rel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
